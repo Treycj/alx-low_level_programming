fd04debbe45b0d5bd909bbd855fc665809481ccd
Tracy Mawanda <mawandatracy@gmail.com>
2022-03-24 15:41:02 -0400

    new changes

---
+++ 1-strncat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+int main(void){
+char dest[98] = "Hello ";
+char src[] = "World!\n";
+char *ptr;
+ ptr = _strncat(s1, s2, 1024);
+putchar(ptr);
+return 0;
+}
+
+
